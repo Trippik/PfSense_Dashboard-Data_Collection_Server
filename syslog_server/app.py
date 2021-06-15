4c93207fc4baa5c5ec7d2e43173c0064f52914c0
Trippik <43576788+Trippik@users.noreply.github.com>
2021-06-15 14:40:16 +0100

    Update app.py
    Final work on the PfSense 2.4.x log parsing and code cleanup/commenting before 0.2 Alpha release.  
    Research has found that the lack of hostname in PfSense 2.4.x syslog data is a bug that was rectified in later versions (https://redmine.pfsense.org/issues/7020)  
      
    Furthermore research has found no easy way to get the original senders IP address from the UDP Syslog packet data when being used in Docker (https://stackoverflow.com/questions/45260132/docker-get-users-real-ip) without the use of more complex traffic routing. As such use of 2.4.x PfSense instances could prove problematic as there is no way to identify data from a specific instance.
    Code to support 2.4.x versions of PfSense will be left in the 0.2 Alpha release (even with it's limited functionality).

--- syslog_server/app.py
+++ syslog_server/app.py
@@ -1,3 +1,7 @@
+#----------------------------------------------------
+#INITIALISATION
+#----------------------------------------------------
+#IMPORT LIBRARIES
 import logging
 import socketserver
 import mysql.connector
@@ -5,17 +9,24 @@ import os
 import re
 import datetime
 
+#ADD TO LOG
 logging.warning("Program Started")
 
+#SET DB PARAMETERS
 db_host = os.environ["DB_IP"]
 db_user = os.environ["DB_USER"]
 db_password = os.environ["DB_PASS"]
 db_schema = os.environ["DB_SCHEMA"]
 db_port = os.environ["DB_PORT"]
 
+#LISTEN ON PORT 514
 HOST, PORT = "0.0.0.0", 514
 
-#Functions to interact with db
+
+#----------------------------------------------------
+#UNDERLYING FUNCTIONS
+#----------------------------------------------------
+#READ FROM DB
 def query_db(query):
     db = mysql.connector.connect(
         host=db_host,
@@ -29,6 +40,7 @@ def query_db(query):
     result = cursor.fetchall()
     return(result)
 
+#WRITE TO DB
 def update_db(query):
     db = mysql.connector.connect(
         host=db_host,
@@ -41,7 +53,7 @@ def update_db(query):
     cursor.execute(query)
     db.commit()
 
-#Functions to handle string manipulation
+#FIND A SUBSTRING WITHIN A STRING
 def element_find(start_char_set, end_char_set, data):
     start = data.find(start_char_set) + len(start_char_set)
     end = data.find(end_char_set)
@@ -51,6 +63,7 @@ def element_find(start_char_set, end_char_set, data):
     sliced_string = data[end_len:]
     return(substring, sliced_string)
 
+#SPLIT A STRING BASED ON NUMBER OF CHARACTERS
 def element_split(no_split, s):
     first_half = s[:no_split]
     second_half = s[no_split:]
@@ -58,34 +71,7 @@ def element_split(no_split, s):
     second_half = second_half.strip()
     return(first_half, second_half)
 
-def log_process_25x(data):
-    result = element_find("<", ">1 ", data)
-    type_code = result[0]
-    result_2 = result[1].split()
-    timestamp = result_2[0]
-    hostname = result_2[1]
-    log_type = result_2[2]
-    rset = result_2[6]
-    rest = result_2[1].strip()  
-    rset = rset.split(",")
-    final_result = (type_code, timestamp, hostname, log_type, rset)
-    return(final_result)
-
-def log_process_24x(data):
-    result = data.split("<", 1)
-    rest = result[1]
-    type_code, rest = rest.split(">", 1)
-    result_2 = element_split(15, rest)
-    timestamp = result_2[0]
-    rest = result_2[1]
-    rest = rest.strip()
-    result_4 = rest.split(":", 1)
-    log_type = result_4[0]
-    rset = result_4[1].strip()
-    rset = rset.split(",")
-    final_result = (type_code, timestamp, log_type, rset)
-    return(final_result)
-
+#REPLACE BLANKS IN TUPLE WITH "NULL"
 def iterate_nulls(tup, mode, count):
     new_tup = []
     if(mode == 1):
@@ -104,14 +90,48 @@ def iterate_nulls(tup, mode, count):
                 new_tup = new_tup + [item]
     return(new_tup)
 
-#Class to handle UDP packets
+
+#----------------------------------------------------
+#PRIMARY FUNCTIONS
+#----------------------------------------------------
+#PARSE PFSENSE 2.5.x LOG DATA
+def log_process_25x(data):
+    result = element_find("<", ">1 ", data)
+    type_code = result[0]
+    result_2 = result[1].split()
+    timestamp = result_2[0]
+    hostname = result_2[1]
+    log_type = result_2[2]
+    rset = result_2[6]
+    rest = result_2[1].strip()  
+    rset = rset.split(",")
+    final_result = (type_code, timestamp, hostname, log_type, rset)
+    return(final_result)
+
+#PARSE PFSENSE 2.4.x LOG DATA
+def log_process_24x(data):
+    result = data.split("<", 1)
+    rest = result[1]
+    type_code, rest = rest.split(">", 1)
+    result_2 = element_split(15, rest)
+    timestamp = result_2[0]
+    rest = result_2[1]
+    rest = rest.strip()
+    result_4 = rest.split(":", 1)
+    log_type = result_4[0]
+    rset = result_4[1].strip()
+    rset = rset.split(",")
+    final_result = (type_code, timestamp, log_type, rset)
+    return(final_result)
+
+#CLASS TO HANDLE INCOMING UDP PACKETS
 class SyslogUDPHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        addr = self.client_address[0]
         data = bytes.decode(self.request[0].strip())
-        socket = self.request[1]
         log = str(data)
+        #Attempt to run data through available parsing functions
         try:
+            #Attempt to parse as PfSense 2.5.x log
             try:
                 logging.warning("PfSense 2.5.x Attempted")
                 results = log_process_25x(log)
@@ -121,6 +141,7 @@ class SyslogUDPHandler(socketserver.BaseRequestHandler):
                 query = query.format(results[0], results[1], results[2], results[3], sub_results[0], sub_results[1], sub_results[2], sub_results[3], sub_results[4], sub_results[5], sub_results[6], sub_results[7], sub_results[8], sub_results[9], sub_results[10], sub_results[11], sub_results[12], sub_results[13], sub_results[14], sub_results[15], sub_results[16], sub_results[17], sub_results[18], sub_results[19], sub_results[20], sub_results[21], sub_results[22])
                 update_db(query)
                 logging.warning("PfSense 2.5.x Completed")
+            #If unable to parse as 2.5.x log parse as 2.4.x log
             except:
                 logging.warning("PfSense 2.4.x Attempted")
                 results = log_process_24x(log)
@@ -132,18 +153,17 @@ class SyslogUDPHandler(socketserver.BaseRequestHandler):
                 query = query.format(results[0], timestamp, results[2], sub_results[0], sub_results[1], sub_results[2], sub_results[3], sub_results[4], sub_results[5], sub_results[6], sub_results[7], sub_results[8], sub_results[9], sub_results[10], sub_results[11], sub_results[12], sub_results[13], sub_results[14], sub_results[15], sub_results[16], sub_results[17], sub_results[18], sub_results[19], sub_results[20], sub_results[21], sub_results[22])
                 update_db(query)
                 logging.warning("PfSense 2.4.x Completed")
+        #If unable to parse save raw log entry to "bucket" within DB
         except:
             query = "INSERT INTO bucket (log) VALUES ('{}')"
             query = query.format(log)
             update_db(query)
             logging.warning("Parsing failed - Adding to log bucket")
 
-
+#MAINLOOP
 if __name__ == "__main__":
 	try:
 		server = socketserver.UDPServer((HOST,PORT), SyslogUDPHandler)
 		server.serve_forever(poll_interval=float(os.environ["POLL_INTERVAL"]))
 	except (IOError, SystemExit):
 		raise
-	except KeyboardInterrupt:
-		print ("Crtl+C Pressed. Shutting down.")
