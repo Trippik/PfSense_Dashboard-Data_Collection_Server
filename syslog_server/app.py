b3159f91eb797fb1c0f420536df50cee258831b5
Trippik <43576788+Trippik@users.noreply.github.com>
2021-07-22 13:44:23 +0100

    Update app.py
    Updated to not add duplicate log entries into DB, or undertake ML checks on them

--- syslog_server/app.py
+++ syslog_server/app.py
@@ -290,14 +290,6 @@ def ipsec_range_subprocess(local_ranges, remote_ranges):
     local_ranges = ipsec_range_secondary_subprocess(local_ranges)
     remote_ranges = ipsec_range_secondary_subprocess(remote_ranges)
     return([local_ranges, remote_ranges])
-
-def interface_sanitize(interfaces):
-    new_interfaces = []
-    for interface in interfaces:
-        if(len(interface) == 9):
-            new_interfaces = new_interfaces + [interface]
-    return(new_interfaces)
-
 #----------------------------------------------------
 #PRIMARY FUNCTIONS
 #----------------------------------------------------
@@ -324,10 +316,6 @@ def standard_ssh_checks():
             process_ipsec_connections(client)
         except:
             logging.warning("IPSec Connections Check Failed")
-        try:
-            interface_process(client)
-        except:
-            logging.warning("Interface Check Failed")
 
 #Check results against ML models
 def ml_check(results, sub_results, pfsense_instance, filename):
@@ -356,6 +344,7 @@ def collect_filter_logs():
     for client in clients:
         parsed = 0
         bucket = 0
+        existing = 0
         logging.warning("---------------------------------------")
         logging.warning("PfSense Instance: " + client[1])
         pfsense_instance = client[0]
@@ -366,7 +355,10 @@ def collect_filter_logs():
                 parsed = parsed + 1
             elif(result == "bucket"):
                 bucket = bucket + 1
+            elif(result == "Existing"):
+                existing = existing + 1
         logging.warning("Logs Parsed: " + str(parsed))
+        logging.warning("Logs Skipped: " + str(existing))
         logging.warning("Logs Added to Bucket: " + str(bucket))
 
 #Collect OpenVPN logs from all clients
@@ -436,14 +428,16 @@ def open_vpn_handle(log, pfsense_instance):
 
 def handle(log, pfsense_instance):
     #Attempt to run data through available parsing functions
+    existing_query = """SELECT COUNT(*) FROM `Dashboard_DB`.`pfsense_logs` WHERE `type_code`= {} AND `record_time`= '{}' AND `pfsense_instance`= {} AND `log_type`= {} AND `rule_number`= {} AND `sub_rule_number`= {} AND `anchor`= {} AND `tracker`= {} AND `real_interface`= {} AND `reason`= {} AND `act`= {} AND `direction`= {} AND `ip_version`= {} AND `tos_header`= {} AND `ecn_header`= {} AND `ttl`= {} AND `packet_id`= {} AND `packet_offset`= {} AND `flags`= {} AND `protocol_id`= {} AND `protocol`= {} AND `packet_length`= {} AND `source_ip`= {} AND `destination_ip`= {} AND `source_port`= {} AND `destination_port`= {} AND `data_length`= {}"""
+    log_insert_query = """INSERT IGNORE INTO `Dashboard_DB`.`pfsense_logs` (`type_code`, `record_time`, `pfsense_instance`, `log_type`, `rule_number`, `sub_rule_number`, `anchor`, `tracker`, `real_interface`, `reason`, `act`, `direction`, `ip_version`, `tos_header`, `ecn_header`, `ttl`, `packet_id`, `packet_offset`, `flags`, `protocol_id`, `protocol`, `packet_length`, `source_ip`, `destination_ip`, `source_port`, `destination_port`, `data_length`, `previous_day_ml_check`, `previous_week_ml_check`, `combined_ml_check`) VALUES ({}, '{}', {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})"""
     try:
-        #Attempt to parse as PfSense 2.5.x log
         try:
+            #Attempt to parse as PfSense 2.5.x log
             results = log_process_25x(log)
             hostname = results[2]
             sub_results = results[4]
             results = [results[0], results[1], str(pfsense_instance), results[3]]
-        #Attempt to parse as PfSense 2.4.x log
+            #Attempt to parse as PfSense 2.4.x log
         except:
             log = log.strip("\n")
             results = log_process_24x(log)
@@ -469,23 +463,19 @@ def handle(log, pfsense_instance):
         sub_results = results_process(sub_results, sub_results_checks, pfsense_instance)
         sub_results = iterate_nulls(sub_results, 2, 99)
         time = datetime.datetime.strptime(results[1], "%Y-%m-%dT%H:%M:%S.%f%z")
-        sanitized_time = results[1].strftime("%Y-%m-%d %H:%M:%S")
-        check_query = """SELECT COUNT(*) FROM `Dashboard_DB`.`pfsense_logs` WHERE `type_code` = {} AND `record_time` = "{}" AND `pfsense_instance` = {} AND `log_type` = {} AND `rule_number` = {} AND `sub_rule_number` = {} AND `anchor` = {} AND `tracker` = {} AND `real_interface` = {} AND `reason` = {} AND `act` = {} AND `direction` = {} AND `ip_version` = {} AND `tos_header` = {} AND `ecn_header` = {} AND `ttl` = {} AND `packet_id` = {} AND `packet_offset` = {} AND `flags` = {} AND `protocol_id` = {} AND `protocol` = {} AND `packet_length` = {} AND `source_ip` = {} AND `destination_ip` = {} AND `source_port` = {} AND `destination_port` = {} AND `data_length` = {}"""
-        logging.warning(check_query.format(results[0], sanitized_time, results[2], results[3], sub_results[0], sub_results[1], sub_results[2], sub_results[3], sub_results[4], sub_results[5], sub_results[6], sub_results[7], sub_results[8], sub_results[9], sub_results[10], sub_results[11], sub_results[12], sub_results[13], sub_results[14], sub_results[15], sub_results[16], sub_results[17], sub_results[18], sub_results[19], sub_results[20], sub_results[21], sub_results[22]))
-        check = query_db(check_query.format(results[0], sanitized_time, results[2], results[3], sub_results[0], sub_results[1], sub_results[2], sub_results[3], sub_results[4], sub_results[5], sub_results[6], sub_results[7], sub_results[8], sub_results[9], sub_results[10], sub_results[11], sub_results[12], sub_results[13], sub_results[14], sub_results[15], sub_results[16], sub_results[17], sub_results[18], sub_results[19], sub_results[20], sub_results[21], sub_results[22]))[0][0]
-        if(check == 0):
+        date_time = time.strftime("%Y-%m-%d %H:%M:%S")
+        existing = query_db(existing_query.format(results[0], date_time, results[2], results[3], sub_results[0], sub_results[1], sub_results[2], sub_results[3], sub_results[4], sub_results[5], sub_results[6], sub_results[7], sub_results[8], sub_results[9], sub_results[10], sub_results[11], sub_results[12], sub_results[13], sub_results[14], sub_results[15], sub_results[16], sub_results[17], sub_results[18], sub_results[19], sub_results[20], sub_results[21], sub_results[22]))[0][0]
+        if(existing == 0):
             daily_ml_result = ml_process(results, sub_results, pfsense_instance, "yesterday")
             weekly_ml_result = ml_process(results, sub_results, pfsense_instance, "last_week")
             if(daily_ml_result == -1 and weekly_ml_result == -1):
                 combined_ml_result = "-1"
             else:
                 combined_ml_result = "1"
-            log_insert_query = """INSERT IGNORE INTO `Dashboard_DB`.`pfsense_logs` (`type_code`, `record_time`, `pfsense_instance`, `log_type`, `rule_number`, `sub_rule_number`, `anchor`, `tracker`, `real_interface`, `reason`, `act`, `direction`, `ip_version`, `tos_header`, `ecn_header`, `ttl`, `packet_id`, `packet_offset`, `flags`, `protocol_id`, `protocol`, `packet_length`, `source_ip`, `destination_ip`, `source_port`, `destination_port`, `data_length`, `previous_day_ml_check`, `previous_week_ml_check`, `combined_ml_check`) VALUES ({}, '{}', {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})"""
-            log_insert_query = log_insert_query.format(results[0], sanitized_time, results[2], results[3], sub_results[0], sub_results[1], sub_results[2], sub_results[3], sub_results[4], sub_results[5], sub_results[6], sub_results[7], sub_results[8], sub_results[9], sub_results[10], sub_results[11], sub_results[12], sub_results[13], sub_results[14], sub_results[15], sub_results[16], sub_results[17], sub_results[18], sub_results[19], sub_results[20], sub_results[21], sub_results[22], daily_ml_result, weekly_ml_result, combined_ml_result)
-            update_db(log_insert_query)
+            update_db(log_insert_query.format(results[0], date_time, results[2], results[3], sub_results[0], sub_results[1], sub_results[2], sub_results[3], sub_results[4], sub_results[5], sub_results[6], sub_results[7], sub_results[8], sub_results[9], sub_results[10], sub_results[11], sub_results[12], sub_results[13], sub_results[14], sub_results[15], sub_results[16], sub_results[17], sub_results[18], sub_results[19], sub_results[20], sub_results[21], sub_results[22], daily_ml_result, weekly_ml_result, combined_ml_result))
             return("parsed")
         else:
-            pass
+            return("Existing")
     except:
         query = """INSERT INTO pfsense_log_bucket (log) VALUES ("{}")"""
         update_db(query.format(log))
@@ -493,7 +483,6 @@ def handle(log, pfsense_instance):
 
 def check_ipsec(client):
     results = run_ssh_command(client, "ipsec statusall")
-    logging.warning(results)
     listening_ip_addresses = []
     connections = []
     shunted_connections = []
@@ -536,16 +525,14 @@ def check_ipsec(client):
 
 def process_ipsec_connections(client):
     connections = check_ipsec(client)[1][1]
-    logging.warning(connections)
+    client_id = str(client[0])
     processed_connections = []
     clear_current_connections_query = """DELETE FROM pfsense_ipsec_connections WHERE pfsense_instance = {}"""
-    update_db(clear_current_connections_query.format(str(client[0])))
+    update_db(clear_current_connections_query.format(client_id))
     connection_insert_query = """INSERT IGNORE INTO pfsense_ipsec_connections (pfsense_instance, local_connection, remote_connection, local_ranges, remote_ranges) VALUES ({}, "{}", "{}", "{}", "{}")"""
-    check_query = """SELECT COUNT(*) FROM pfsense_ipsec_connections WHERE pfsense_instance = {} AND local_connection = "{}" AND remote_connection = "{}" AND local_ranges = "{}" AND remote_ranges = '{}'"""
     for row in connections:
         prefix = row.split(":", 1)
         if(prefix[0].strip()[:3] == "con"):
-            logging.warning("Connection Found")
             details = prefix[1]
             intermediary = details.split(":", 1)
             start = intermediary[0].strip()
@@ -553,10 +540,8 @@ def process_ipsec_connections(client):
                 entry = intermediary[1]
                 if(start == "local"):
                     local = entry
-                    local = local.split("]", 1)[0].split("[", 1)[1]
                 elif(start == "remote"):
                     remote = entry
-                    remote = remote.split("]", 1)[0].split("[", 1)[1]
                 elif(start == "child"):
                     child = entry.strip()
                     splits = child.split(" === ")
@@ -568,76 +553,10 @@ def process_ipsec_connections(client):
                     remote_ranges = remote_ranges.strip().split("|/0")
                     local_ranges = remove_empty_entries(local_ranges)
                     remote_ranges = remove_empty_entries(remote_ranges)
-                logging.warning(check_query.format(str(client[0]), local, remote, local_ranges, remote_ranges))
-                existing = query_db(check_query.format(str(client[0]), local, remote, local_ranges, remote_ranges))[0][0]
-                logging.warning(existing)
-                if(existing == 0):
-                    update_db(connection_insert_query.format(str(client[0]), local, remote, local_ranges, remote_ranges))
-                    processed_connections = processed_connections + [[local, remote, local_ranges, remote_ranges]]
-                else:
-                    pass
+                update_db(connection_insert_query.format(client_id, local, remote, local_ranges, remote_ranges))
+                processed_connections = processed_connections + [[local, remote, local_ranges, remote_ranges]]
             except:
                 pass
-    logging.warning(processed_connections)
-
-def interface_check(client):
-    raw_results = run_ssh_command(client, "ifconfig -a")
-    interfaces = []
-    new_interface = []
-    count = 0
-    for row in raw_results:
-        if("\n" and not "\t" in row):
-            interfaces = interfaces + [new_interface]
-            interface = row.split(":", 1)[0]
-            new_interface = [interface]
-            count = count + 1
-        elif("\n" and "\t" in row):
-            if('\tdescription' in row):
-                result = row.split(":", 1)[1].strip(" ").strip("\n")
-            elif('\tether' in row):
-                result = row.split(" ", 1)[1].strip(" ").strip("\n")
-            elif('\toptions=' in row):
-                result = row.split("=", 1)[1].strip(" ").strip("\n")
-            elif('\tinet6' in row):
-                result = row.split(" ", 1)[1].split("%")[0]
-            elif('\tinet' in row):
-                result = row.split(" ", 1)[1].split(" netmask")[0]
-            elif('\tmedia' in row):
-                result = row.split(": ", 1)[1].strip("\n")
-            elif('\tstatus' in row):
-                result = row.split(": ", 1)[1].strip("\n")
-            else:
-                pass
-            new_interface = new_interface + [result]
-    interfaces = interface_sanitize(interfaces)
-    return(interfaces)
-
-def interface_process(client):
-    interfaces = interface_check(client)
-    clearing_query = "DELETE FROM pfsense_instance_interfaces WHERE pfsense_instance = {}"
-    query = """INSERT IGNORE INTO `pfsense_instance_interfaces`
-(`pfsense_instance`,
-`interface_name`,
-`interface_description`,
-`interface_attributes`,
-`mac_address`,
-`ipv6_address`,
-`ipv4_address`,
-`interface_type`,
-`interface_status`)
-VALUES
-({},
-"{}",
-"{}",
-"{}",
-"{}",
-"{}",
-"{}",
-"{}",
-"{}")"""
-    update_db(clearing_query.format(str(client[0])))
-    for interface in interfaces:
-        update_db(query.format(client[0], interface[0], interface[1], interface[2], interface[3], interface[4], interface[5], interface[6], interface[6]))
 
 #MAINLOOP
 loop = True
@@ -657,4 +576,3 @@ while(loop == True):
         except:
             logging.warning("SSH POLL ERROR")
     time.sleep(int(os.environ["SYSLOG_POLL_INTERVAL"]))
-
