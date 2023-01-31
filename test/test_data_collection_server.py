647ddcbacde3d4004606e33f93ab0864bcd89458
Cameron Trippick <trippickc@gmail.com>
2023-01-30 22:14:36 -0500

    Adding test for data_hanlder.remove_empty_entries()

--- test/test_data_collection_server.py
+++ test/test_data_collection_server.py
@@ -1,6 +1,9 @@
 import pytest
 from syslog_server.lib import client_handler, data_handler
 
+example_list = ["Beep", "Boop", "", ""]
+example_str = "<BlahBlahBlah>1"
+
 # --------------------------------
 # data_handler tests
 # --------------------------------
@@ -15,14 +18,15 @@ def test_row_sanitize():
     assert result_3 == goal
 
 def test_element_find():
-    example = "<BlahBlahBlah>1"
-    assert data_handler.element_find("<", ">1", example) == ('BlahBlahBlah', '')
-    assert data_handler.element_find("<", ">", example) == ("BlahBlahBlah", '1')
+    assert data_handler.element_find("<", ">1", example_str) == ('BlahBlahBlah', '')
+    assert data_handler.element_find("<", ">", example_str) == ("BlahBlahBlah", '1')
 
 def test_element_split():
-    example = "<BlahBlahBlah>1"
-    assert data_handler.element_split(5, example) == ("<Blah", "BlahBlah>1")
+    assert data_handler.element_split(5, example_str) == ("<Blah", "BlahBlah>1")
 
 def test_iterate_nulls():
-    example = ["Beep", "Boop", "", ""]
-    assert data_handler.iterate_nulls(example, 2, 0) == ["Beep", "Boop", "NULL", "NULL"]
+    assert data_handler.iterate_nulls(example_list, 2, 0) == ["Beep", "Boop", "NULL", "NULL"]
+
+def test_remove_empty_entries():
+    assert data_handler.remove_empty_entries(example_list) == ['Beep', 'Boop']
+
